8c7adcb9a519b0c03beca858e88301455b3f50e9
Dima Gerasimov <karlicoss@gmail.com>
2019-09-03 01:00:45 +0100

    add my/search-code

--- config.el
+++ config.el
@@ -6,8 +6,25 @@
   (insert (format-time-string "[%Y-%m-%d %H:%M]")))
 
 
+; TODO need to ignore # files?
+(defun my/helm-files-do-rg-recursive (where)
+  (let ((helm-ag-command-option "--follow"))
+    (spacemacs/helm-files-do-rg where)))
 
 
+(defun my/search-code ()
+  (interactive)
+  (my/helm-files-do-rg-recursive my/code-search-path)) ; my/code-search-path defined externally
+; TODO maybe import it from this file? doesn't hurt if load twice?..
+
+
+
+; keybindings etc
 
 
 (evil-global-set-key 'insert (kbd "C-t") 'my/now)
+
+
+
+(spacemacs/set-leader-keys
+  "S c" 'my/search-code)

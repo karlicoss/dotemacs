fcaf3de15aa1eef643c22aa2bd7a45e90a939137
Dima Gerasimov <karlicoss@gmail.com>
2019-09-05 23:10:41 +0100

    Update config.el

--- config.el
+++ config.el
@@ -1,5 +1,21 @@
 ; refined init.el, gradually will mode all of my config here
 
+
+; (cl-defun org-files-in (path &key (archive nil))
+;   (let ((regex (if archive "\\.\\(org\\|org\_archive\\)$" "\\.org$"))
+;         (is-lockfile (lambda (s) (string-match-p "/\\.#.*" s)))
+;     (remove-if is-lockfile (directory-files-recursively path regex))
+
+(cl-defun org-files-in (path &key (archive nil) (follow nil))
+  "This is a bit horrible, but none of standard functions or popular libs support following symlink :("
+  (let* ((patterns (if archive "-g '*.org' -g '*.org_archive'" "-g '*.org'"))
+         (follows (if follow "--follow" ""))
+         (rg-command (format "rg --files %s -0 %s %s" follows patterns path)))
+    (s-split "\0" (shell-command-to-string rg-command) t)))
+
+; helper functions
+
+
 (defun my/now ()
   "Insert current timestamp in org-mode format"
   (interactive)
@@ -7,19 +23,36 @@
 
 
 ; TODO need to ignore # files?
-(defun my/helm-files-do-rg-recursive (where)
+(defun --my/helm-files-do-rg-follow (where)
   (let ((helm-ag-command-option "--follow"))
     (spacemacs/helm-files-do-rg where)))
 
 
-(defun my/search-info ()
+(defun my/search-notes ()
   (interactive)
-  (my/helm-files-do-rg-recursive my/info-search-path)) ; path defined externally
+  (--my/helm-files-do-rg-follow my/notes-targets))
 
 
 (defun my/search-code ()
   (interactive)
-  (my/helm-files-do-rg-recursive my/code-search-path)) ; path defined externally
+  (--my/helm-files-do-rg-follow my/code-targets))
+
+
+(defun --my/drill-with-tag (tag)
+  (require 'org-drill)
+  (let ((org-drill-question-tag tag))
+    (org-drill (org-files-in my/drill-targets :follow t))))
+
+
+(defun my/habits ()
+  (interactive)
+  (--my/drill-with-tag "habit"))
+
+
+(defun my/drill ()
+  (interactive)
+  (--my/drill-with-tag "drill"))
+
 
 
 
@@ -29,9 +62,9 @@
 
 
 (spacemacs/set-leader-keys
-  "S i" #'my/search-info
+  "S i" #'my/search-notes
   "S c" #'my/search-code)
 
 
-(global-set-key (kbd "<f1>") #'my/search-info)
+(global-set-key (kbd "<f1>") #'my/search-notes)
 (global-set-key (kbd "<f3>") #'my/search-code)

95a43a6ce9b17e9c23156ce977d2fe01d3279701
Dima Gerasimov <karlicoss@gmail.com>
2019-10-18 00:18:12 +0100

    refactor file search function

--- .spacemacs.d/config.el
+++ .spacemacs.d/config.el
@@ -1,32 +1,28 @@
 ; refined init.el, gradually will mode all of my config here
 
 
-; (cl-defun org-files-in (path &key (archive nil))
-;   (let ((regex (if archive "\\.\\(org\\|org\_archive\\)$" "\\.org$"))
-;         (is-lockfile (lambda (s) (string-match-p "/\\.#.*" s)))
-;     (remove-if is-lockfile (directory-files-recursively path regex))
-
-(cl-defun org-files-in (path &key (archive nil) (follow nil))
-  "This is a bit horrible, but none of standard functions or popular libs support following symlink :("
-  (let* ((patterns (if archive "-g '*.org' -g '*.org_archive'" "-g '*.org'"))
+;;; searching for things
+(cl-defun my/files-in (path &key (exts nil) (follow nil))
+  "This is a bit horrible, but none of standard Elisp functions or popular libs support following symlink :("
+  (let* ((patterns (s-join " " (-map (lambda (i) (format "-g '*.%s'" i)) exts)))
          (follows (if follow "--follow" ""))
          (rg-command (format "rg --files %s -0 %s %s" follows patterns path)))
     (-map #'file-truename (s-split "\0" (shell-command-to-string rg-command) t))))
 
-; helper functions
+(cl-defun my/org-files-in (path &key (archive nil) (follow nil))
+  (my/files-in path :exts (if archive '("org" "org_archive") '("org")) :follow follow))
 
 
-(defun my/now ()
-  "Insert current timestamp in org-mode format"
-  (interactive)
-  (insert (format-time-string "[%Y-%m-%d %a %H:%M]")))
-
-
-;;; searching for things
 ; TODO need to ignore # files?
 (defun --my/helm-files-do-rg-follow (where)
   (let ((helm-ag-command-option "--follow"))
     (spacemacs/helm-files-do-rg where)))
+
+
+(defun --my/find-file-read-only-defensive (f)
+  "Convenient to ignore lock files and generally race conditions"
+  (with-demoted-errors
+      (find-file-read-only f)))
 
 
 (defun my/search ()
@@ -39,14 +35,14 @@
   (spacemacs/helm-files-do-rg "/" (my/code-targets)))
 
 
-(defun --my/find-file-read-only-defensive (f)
-  "Convenient to ignore lock files and generally race conditions"
-  (with-demoted-errors
-      (find-file-read-only f)))
-
 (defun my/prepare-swoop ()
   "Swoop only works in open buffers apparently. So this opens all notes in buffers..."
-  (let ((files (org-files-in my/search-targets :archive t :follow t))
+  (let ((files (my/files-in my/search-targets
+                            :follow t
+                            :exts '(
+                                    "org" "org_archive"
+                                    "txt"
+                                    "page" "md" "markdown"))) ; older Markdown/gitit notes
         ; disable local variables, mainly so nothing prompts while opening org files with babel
         (enable-local-variables nil)
         (enable-local-eval nil)
@@ -61,7 +57,7 @@
 (defun --my/drill-with-tag (tag)
   (require 'org-drill)
   (let ((org-drill-question-tag tag))
-    (org-drill (org-files-in my/drill-targets :follow t))))
+    (org-drill (my/org-files-in my/drill-targets :follow t))))
 
 (defun my/habits ()
   (interactive)
@@ -78,7 +74,7 @@
 ;;; org-agenda
 
 (defun get-org-agenda-files ()
-  (org-files-in my/agenda-targets :follow t))
+  (my/org-files-in my/agenda-targets :follow t))
 
 ; TODO hotkey to toggle private/non private?
 (defun my/agenda (&optional arg)
@@ -97,12 +93,20 @@
 ;;; org-refile
 
 (defun get-org-refile-targets ()
-  (org-files-in my/refile-targets :follow t))
+  (my/org-files-in my/refile-targets :follow t))
 
 ;;;
 
 
-; keybindings etc
+;;; misc stuff
+(defun my/now ()
+  "Insert current timestamp in org-mode format"
+  (interactive)
+  (insert (format-time-string "[%Y-%m-%d %a %H:%M]")))
+;;;
+
+
+;;; keybindings etc
 
 (evil-global-set-key 'insert (kbd "C-t") #'my/now)
 
@@ -120,3 +124,4 @@
 
 (global-set-key (kbd "<f1>") #'my/search)
 (global-set-key (kbd "<f3>") #'my/search-code)
+;;;

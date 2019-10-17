c335dcfdcb5b2bbe4fbf873affdb6df73f998f8c
Dima Gerasimov <karlicoss@gmail.com>
2019-10-17 23:36:42 +0100

    patch spacemacs/helm-files-do-rg to accept extra argument

--- patch-helm.el
+++ patch-helm.el
@@ -1,6 +1,11 @@
-;; ugh. need to pass targets argument...
+; -*- mode: Lisp; paredit-mode: 0; -*-
 
-(defun spacemacs/helm-files-do-rg (&optional dir targets)
+
+; need to pass down 'targets' argument
+; TODO commit it to spacemacs?
+(el-patch-feature helm)
+(with-eval-after-load 'helm
+(el-patch-defun spacemacs/helm-files-do-rg (&optional dir targets)
   "Search in files with `rg'."
   (interactive)
   ;; --line-number forces line numbers (disabled by default on windows)
@@ -10,4 +15,6 @@
          (helm-ag-base-command (if spacemacs-helm-rg-max-column-number
                                    (concat root-helm-ag-base-command " --max-columns " (number-to-string spacemacs-helm-rg-max-column-number))
                                  root-helm-ag-base-command)))
-    (helm-do-ag dir targets)))
+    (helm-do-ag dir (el-patch-add targets))))
+
+)

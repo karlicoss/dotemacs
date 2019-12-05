51d130d08eb52ff01f6d5081610cf6a980b4ebda
Dima Gerasimov <karlicoss@gmail.com>
2019-12-05 22:49:27 +0000

    configure python lsp

--- lang/python.el
+++ lang/python.el
@@ -5,3 +5,9 @@
     '((multiline   . backslash)
       (max_columns . 150))))
 
+(with-eval-after-load 'lsp-mode
+  (setq lsp-pyls-plugins-pylint-args '("--errors-only"))
+  (setq lsp-pyls-plugins-pyflakes-enabled nil))
+
+; TODO disable live mode for mypy?
+; https://github.com/warchiefx/dotemacs/blob/e71c779f28955eaa234c097afd6e492ec3ba642e/site-wcx/wcx-lsp.el#L20

dd4c0c0044467647ef5c6ffeeb6bf50f7565f370
Dima Gerasimov <karlicoss@gmail.com>
2019-12-26 17:19:14 +0000

    use my version org org-sync

--- .spacemacs.d/bootstrap.el
+++ .spacemacs.d/bootstrap.el
@@ -1,9 +1,11 @@
 (setq my/spacemacs-layers
-      '((colors :variables colors-enable-nyan-cat-progress-bar t)
+      '(
+        (colors :variables colors-enable-nyan-cat-progress-bar t)
         ))
 
 (setq my/additional-packages
-      '(el-patch
+      '(
+        el-patch
 
         xclip
         sync-recentf
@@ -11,13 +13,20 @@
         org-plus-contrib
         org-super-agenda
         org-ql
-        org-sync
+        (org-sync
+         :location (recipe
+                    :fetcher github
+                    :repo "karlicoss/org-sync"
+                    :branch "fix-nondeterminsm"))
 
         helm-rg
 
-        (lsp-haskell :location (recipe :fetcher github :repo "emacs-lsp/lsp-haskell"))
+        (lsp-haskell
+         :location (recipe
+                    :fetcher github :repo "emacs-lsp/lsp-haskell"))
         ))
 
 (setq my/excluded-packages
-      '(org-projectile ; disabled due to some stupid bug https://github.com/syl20bnr/spacemacs/issues/9374
+      '(
+        org-projectile ; disabled due to some stupid bug https://github.com/syl20bnr/spacemacs/issues/9374
         ))

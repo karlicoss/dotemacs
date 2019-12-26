caab3fad772ee35da38027a9a949fcd0c8294fd4
Dima Gerasimov <karlicoss@gmail.com>
2019-12-26 12:02:44 +0000

    add bootstrap.el with stuff that spacemacs loads at configuration time

---
+++ .spacemacs.d/bootstrap.el
@@ -0,0 +1,23 @@
+(setq my/spacemacs-layers
+      '((colors :variables colors-enable-nyan-cat-progress-bar t)
+        ))
+
+(setq my/additional-packages
+      '(el-patch
+
+        xclip
+        sync-recentf
+
+        org-plus-contrib
+        org-super-agenda
+        org-ql
+        org-sync
+
+        helm-rg
+
+        (lsp-haskell :location (recipe :fetcher github :repo "emacs-lsp/lsp-haskell"))
+        ))
+
+(setq my/excluded-packages
+      '(org-projectile ; disabled due to some stupid bug https://github.com/syl20bnr/spacemacs/issues/9374
+        ))

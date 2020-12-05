181f37a9a99bd981e59e795ae2da12a011f83eb7
Dima Gerasimov <karlicoss@gmail.com>
2020-12-05 22:09:18 +0000

    add pyls+mypy instructions

--- lang/python.el
+++ lang/python.el
@@ -9,5 +9,13 @@
   (setq lsp-pyls-plugins-pylint-args '("--errors-only"))
   (setq lsp-pyls-plugins-pyflakes-enabled nil))
 
+; unclear what is different between pyls and mspyls? the latter is native and in C#, apparently features are the same though
+; NOTE: need extra 
+; pipx install python-language-server
+; pipx inject  python-language-server pyls-mypy
+; see https://github.com/tomv564/pyls-mypy/issues/37#issuecomment-695963210
+; pipx inject  python-language-server future
+
+
 ; TODO disable live mode for mypy?
 ; https://github.com/warchiefx/dotemacs/blob/e71c779f28955eaa234c097afd6e492ec3ba642e/site-wcx/wcx-lsp.el#L20
